d34916b4f098b8ad43b103c25b212f03cba7676c
Isabel Restrepo (pencil) <isabelrestre@gmail.com>
2018-02-05 15:41:31 -0500

    .jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs()
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),     
-    repo = "github.com/mirestrepo/EEGTest.git",
+    repo = "github.com/mirestrepo/EEGTest.jl.git",
     julia  = "0.6",
     osname = "linux"
 )
