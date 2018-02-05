9ce2b79e6f44e0d1ab7a73a0f83923638b7edaab
Isabel Restrepo (pencil) <isabelrestre@gmail.com>
2018-02-05 15:33:10 -0500

    Deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, EEGTest
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),     
+    repo = "github.com/mirestrepo/EEGTest.git",
+    julia  = "0.6",
+    osname = "linux"
+)
